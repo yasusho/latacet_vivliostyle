4edfd99c01a02a7f0a9f6f2857cd437904ab9a74
yasusho <yasusho1020@gmail.com>
2025-11-08 02:50:56 +0900

    svgをgit LFSで管理

---
+++ raikhruiuet/rulebook/vivliostyle.config.js
@@ -0,0 +1,13 @@
+// @ts-check
+/** @type {import('@vivliostyle/cli').VivliostyleConfigSchema} */
+const vivliostyleConfig = {
+  title: 'raikhruiuet',
+  author: 'author',
+  language: 'x-praige',
+  theme: 'style.css',
+  size: 'JIS-B5 landscape',
+  entry: [
+    "rule.md"
+  ]
+};
+module.exports = vivliostyleConfig;
